d3ced6ff7ed2d2b3b676fc59e0f36a89795a85dc
Влад Шарапа <vl.sharapa20@gmail.com>
2023-06-03 13:23:26 +0300

    Add task create view

--- backend/todo_project/todo_app/urls.py
+++ backend/todo_project/todo_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.overview, name='test'),
-    path('task_list/', views.task_list, name='task_list')
+    path('task_list/', views.task_list, name='task_list'),
+    path('task_create/', views.CreateTask.as_view(), name='task_create'),
 ]
--- backend/todo_project/todo_app/views.py
+++ backend/todo_project/todo_app/views.py
@@ -1,13 +1,19 @@
-from rest_framework.decorators import api_view
-from django.shortcuts import render
-from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+
 
 # Create your views here.
 
 @api_view(['GET'])
 def overview(request):
+    """Get available urls"""
+
     urls = {
         '1': 'first',
     }
@@ -15,7 +21,23 @@ def overview(request):
 
 @api_view(['GET'])
 def task_list(request):
+    """Output all tasks"""
+
     tasks = Task.objects.all()
     serializer = TaskSerializer(tasks, many=True)
     
     return Response(serializer.data)
+
+
+class CreateTask(generics.GenericAPIView):
+    serializer_class = TaskSerializer
+
+    def post(self, request):
+        serializer = TaskSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+        
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
